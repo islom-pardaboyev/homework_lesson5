e566c65a0f8ed49e4afb2a7ea453f9aafb6ee3dd
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-10-08 19:45:07 +0500

    init

--- src/app/(layout)/sidebar/page.jsx
+++ src/app/(layout)/sidebar/page.jsx
@@ -2,9 +2,12 @@
 import React, { useContext } from "react";
 import { Context } from "../../context/MainContext";
 import { CloseSidebarIcon } from "../../assets/svgs";
+import { FaCircleXmark } from "react-icons/fa6";
+import { toast } from "react-toastify";
 
 function Sidebar() {
-  const { showSidebar, setShowSidebar, savedProducts } = useContext(Context);
+  const { showSidebar, setShowSidebar, savedProducts, setSavedProducts } =
+    useContext(Context);
   console.log(showSidebar);
   return (
     <section
@@ -28,16 +31,34 @@ function Sidebar() {
         </header>
         <span className="w-full block h-px bg-gray mt-[26px] mb-[42px]"></span>
         <div className="grid grid-cols-1 space-y-[20px]">
-        {savedProducts.map(item =>  
+          {savedProducts.length ? savedProducts.map((item) => (
             <div className="col-span-1 flex items-center gap-2">
-                <img src={item.images[0]} className="w-[108px] h-[105px] bg-primary rounded-[10px]" alt="" />
-                <div className="flex flex-col ">
-                    <h1 className="line-clamp-1 font-normal">{item.title}</h1>
-                    <p className="font-bold text-xs text-primary_text_color">Rs. {item.price}</p>
-                </div>
+              <img
+                src={item.images[0]}
+                className="w-[108px] h-[105px] bg-primary object-contain rounded-[10px]"
+                alt=""
+              />
+              <div className="flex flex-col ">
+                <h1 className="line-clamp-1 font-normal">{item.title}</h1>
+                <p className="font-bold text-xs text-primary_text_color">
+                  Rs. {item.price}
+                </p>
+                <p
+                  className="cursor-pointer mt-3 text-gray"
+                  onClick={() => {
+                    setSavedProducts((prevProducts) =>
+                      prevProducts.filter((product) => product.id !== item.id)
+                    );
+                    toast.success('Product Deleted')
+                  }}
+                >
+                  <FaCircleXmark />
+                </p>
+              </div>
             </div>
-        )}
+          )) : <p className="text-center">Not yet products...</p>}
         </div>
+        <p className="capitalize font-bold mt-10 border-t">total: {(savedProducts.reduce((acc, curr) => acc + curr.price, 0)).toFixed(2)}$</p>
       </div>
     </section>
   );
